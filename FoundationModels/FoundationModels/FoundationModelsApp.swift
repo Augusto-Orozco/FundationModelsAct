08ed4e9249a44a208c379957b6b110de68baff37
Augusto Orozco <a00839579@tec.mx>
2025-11-13 11:25:24 -0600

    AppStart

---
+++ FoundationModels/FoundationModels/FoundationModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsApp.swift
+//  FoundationModels
+//
+//  Created by Alumno on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoundationModels/FoundationModelsTests/FoundationModelsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsTests.swift
+//  FoundationModelsTests
+//
+//  Created by Alumno on 13/11/25.
+//
+
+import Testing
+@testable import FoundationModels
+
+struct FoundationModelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
